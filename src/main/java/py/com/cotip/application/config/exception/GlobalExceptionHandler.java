f80fca96ed76675ecab439633ff1471f683f00ee
AdanLedesma66 <adagalese@gmail.com>
2024-11-09 04:17:25 -0300

    feat: notfound mapped

--- src/main/java/py/com/cotip/application/config/exception/GlobalExceptionHandler.java
+++ src/main/java/py/com/cotip/application/config/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package py.com.cotip.application.config.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,29 +8,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import py.com.cotip.application.config.model.CotipResponse;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // ::: global exception
+    // ::: CotipException handler
 
     @ExceptionHandler(CotipException.class)
     public ResponseEntity<Map<String, Object>> handleCotipException(CotipException ex) {
         return ResponseEntity.status(ex.getHttpStatus()).body(CotipResponse.responseError(ex));
     }
 
-    // ::: generic exception
+    // ::: Generic exception handler
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        String errorCode = "UNKNOWN_ERROR";
+        String errorMessage = "An unexpected error occurred";
+
+        if (ex instanceof NoSuchElementException || ex instanceof EntityNotFoundException) {
+            status = HttpStatus.NOT_FOUND;
+            errorCode = "NOT_FOUND";
+            errorMessage = "The requested resource was not found";
+        }
+
         CotipException genericException = new CotipException(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "UNKNOWN_ERROR",
-                "An unexpected error occurred",
+                status.value(),
+                errorCode,
+                errorMessage,
                 null,
                 false,
                 ex
         );
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(CotipResponse.responseError(genericException));
+        return ResponseEntity.status(status).body(CotipResponse.responseError(genericException));
     }
 }
