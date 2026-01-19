8e31d1fd9eef06e455d9379a486f08176acfb4e2
AdanLedesma66 <adagalese@gmail.com>
2026-01-19 12:52:13 -0300

    refac: pasamos a java 21 y spring boot 4.x

--- src/main/java/py/com/cotip/external/webservice/CotipOutPortImpl.java
+++ src/main/java/py/com/cotip/external/webservice/CotipOutPortImpl.java
@@ -1,8 +1,6 @@
 package py.com.cotip.external.webservice;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -20,6 +18,9 @@ import py.com.cotip.external.webservice.model.ContinentalExternal;
 import py.com.cotip.external.webservice.model.ListadoGbnExternal;
 import py.com.cotip.external.webservice.model.RioExternal;
 import py.com.cotip.external.webservice.util.CurrencyUtils;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
