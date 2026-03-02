aedb2e562cd1ef0e5a337c4fd144f9a72b665bad
AdanLedesma66 <adagalese@gmail.com>
2026-03-02 17:20:36 -0300

    versioning 1.0

---
+++ infrastructure/src/main/java/py/com/cotip/insfrastructure/config/CotipConstants.java
@@ -0,0 +1,10 @@
+package py.com.cotip.insfrastructure.config;
+
+public final class CotipConstants {
+
+    public static final String VERSION_V1 = "1.0";
+    public static final String COTIP_API_VERSION_HEADER = "cotip-api-version";
+
+    private CotipConstants() {
+    }
+}
